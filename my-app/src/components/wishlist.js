bfc957b4f9145cb57c7192532fba3b289c4f6d04
soham-parab <parabsoham@gmail.com>
2021-04-15 23:48:05 +0530

    wishlist functionality for products

--- my-app/src/components/wishlist.js
+++ my-app/src/components/wishlist.js
@@ -3,5 +3,13 @@ import { useState, useReducer } from "react";
 import { useWishlist } from "../contexts/WishlistContext";
 
 export function Wishlist() {
-return null
-}
+    const { itemsInWishlist } = useWishlist();
+ 
+    return (
+       <div>
+          {itemsInWishlist.map((items) => {
+             return <h1>{items}</h1>;
+          })}
+       </div>
+    );
+ }
