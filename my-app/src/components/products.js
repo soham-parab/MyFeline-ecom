7710860c1ee39a9fd5d120bf3ae79099de1d0c17
soham-parab <parabsoham@gmail.com>
2021-04-15 23:46:19 +0530

    listed products from faker on Productlisting page

--- my-app/src/components/products.js
+++ my-app/src/components/products.js
@@ -7,8 +7,51 @@ import { useWishlist } from "../contexts/WishlistContext";
 
 import "./components.css"
 
-export function ProductListing(){
-    return null
-
-
-}
+export function ProductListing() {
+    const { products, setProducts } = useProduct();
+    const { setItemsInCart } = useCart();
+    const { setItemsInWishlist } = useWishlist();
+    useEffect(() => {
+       (async function () {
+          const productData = await axios.get("../api/products");
+          setProducts(productData.data.products);
+       })();
+    }, []);
+ 
+    return (
+       <div>
+          <h1>Products!</h1>
+          {products.map((item) => {
+             return (
+                <div key={item.id}>
+                   <p className="productName">{item.name}</p>
+                   <img className="productImage" src={item.image} alt="nothing"></img>
+                   <p className ="itemPrice">{item.price}</p>
+                   <div className = "cartDiv">
+                      <button className = "cartButton"
+                         onClick={() =>
+                            setItemsInCart((prd) => [...prd, item.name,item.price,item.image])
+                         }
+                      >
+                         Add to Cart
+                      </button>
+                   </div>
+                   <div className = "wishListDiv">
+                      <button  className = "wishListButton"
+                         onClick={() =>
+                            setItemsInWishlist((prd) => [
+                               ...prd,
+                               item.name,
+                               item.price,
+                            ])
+                         }
+                      >
+                         Add to Wishlist
+                      </button>
+                   </div>
+                </div>
+             );
+          })}
+       </div>
+    );
+ }
