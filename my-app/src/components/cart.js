1dd280dd43865912456e5d54f0d949362f416360
soham-parab <parabsoham@gmail.com>
2021-04-16 00:12:24 +0530

    code refactoring

--- my-app/src/components/cart.js
+++ my-app/src/components/cart.js
@@ -3,17 +3,17 @@ import { useState, useReducer } from "react";
 import { useCart } from "../contexts/CartContext";
 
 export function Cart() {
-    const { itemsInCart, setItemsInCart } = useCart();
-    console.log(itemsInCart);
-    return (
-       <div>
-          {itemsInCart.map((items) => {
-             return (
-                <div>
-                   <h1>{items}</h1>
-                </div>
-             );
-          })}
-       </div>
-    );
- }
+   const { itemsInCart, setItemsInCart } = useCart();
+   console.log(itemsInCart);
+   return (
+      <div>
+         {itemsInCart.map((items) => {
+            return (
+               <div>
+                  <h1>{items}</h1>
+               </div>
+            );
+         })}
+      </div>
+   );
+}
