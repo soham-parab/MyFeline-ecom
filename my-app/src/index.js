83bc8adf9578301086b6009ffb8ffeb4ee9f8bc7
soham-parab <parabsoham@gmail.com>
2021-04-15 23:17:44 +0530

    added mockserver using faker and mirage

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import setupMockServer from "../src/api/mock-server"
 import { CartProvider } from "./contexts/CartContext";
 import { ProductProvider } from "./contexts/ProductsContext";
 import { WishlistProvider } from "./contexts/WishlistContext";
 
+setupMockServer()
 
 
 ReactDOM.render(
