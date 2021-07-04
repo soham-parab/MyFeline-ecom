bb6fca53dfdefdf1912d6fc4d5a3b11582d4e798
soham-parab <57428835+soham-parab@users.noreply.github.com>
2021-07-05 03:10:38 +0530

    added Private Routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,3 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useLocation,
-  Navigate,
-  useNavigate,
-} from "react-router-dom";
 import { ProductListing } from "../src/Pages/Products/products";
 import "./App.css";
 import { Cart } from "./Pages/Cart/cart";
@@ -17,36 +8,16 @@ import { Homepage } from "./Pages/Homepage/Homepage";
 import { Login } from "./Pages/Login/Login";
 import { Register } from "./Pages/Register/Register";
 import { useAuth } from "./contexts/AuthContext";
+import { Router } from "./components/Routes/router";
 
 function App() {
   const { auth } = useAuth();
-  // const { navigate } = useNavigate();
 
-  // function PrivateRoute({ path, ...props }) {
-  //   console.log(path, props, "llllllll");
-  //   return auth ? (
-  //     <Route {...props} path={path} />
-  //   ) : (
-  //     <Navigate state={{ from: path }} replace to="/login" />
-  //   );
-  // }
   return (
     <div className="App">
       <div className="header"></div>
 
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/products" element={<ProductListing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/wishlist" element={<Wishlist />} />
-          <Route path="/productpage/:id" element={<ProductPage />} />
-          <Route path="/productpage/:id" element={<ProductPage />} />
-        </Routes>
-      </Router>
+      <Router />
     </div>
   );
 }
---
+++ my-app/src/components/Routes/router.js
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useNavigate,
+  useParams,
+  useLocation,
+} from "react-router-dom";
+import { Nav } from "../nav/nav";
+import { ProductPage } from "../../Pages/Productpage/productpage";
+import { Cart } from "../../Pages/Cart/cart";
+import { Wishlist } from "../../Pages/Wishlist/wishlist";
+import { Login } from "../../Pages/Login/Login";
+import { Register } from "../../Pages/Register/Register";
+import { useAuth } from "../../contexts/AuthContext";
+import { ProductListing } from "../../Pages/Products/products";
+import { Homepage } from "../../Pages/Homepage/Homepage";
+
+export function Router() {
+  const { auth } = useAuth();
+
+  function PrivateRoute({ path, ...props }) {
+    console.log(path, props, "llllllll");
+    return auth ? (
+      <Route {...props} path={path} />
+    ) : (
+      <Navigate state={{ from: path }} replace to="/login" />
+    );
+  }
+
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <PrivateRoute path="/cart" element={<Cart />} />
+        <PrivateRoute path="/wishlist" element={<Wishlist />} />
+        <Route path="/productpage/:id" element={<ProductPage />} />
+      </Routes>
+    </>
+  );
+}
